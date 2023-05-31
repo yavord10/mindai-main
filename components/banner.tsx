32b35dd62e8aaeb20c316c8dbb20d2217c7e2cf9
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-05-31 22:10:35 +0100

    More changes

--- components/banner.tsx
+++ components/banner.tsx
@@ -9,10 +9,10 @@ export default function Banner() {
     <>
       {bannerOpen && (
         <div
-          className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-60"
-          style={{ bottom: "100px", right: "120px" }}
+          className="fixed bottom-0 right-0 md:bottom-8 md:right-12 md:w-auto z-60"
+          style={{ bottom: "100px", right: "5rem" }}
         >
-          <div className="bg-slate-800 text-slate-50 text-sm p-3 shadow-lg flex justify-between rounded-lg">
+          <div className="bg-slate-800 text-slate-50 text-sm p-3 shadow-lg flex rounded-lg">
             <div className="text-slate-500 inline-flex">
               <a
                 className="font-medium hover:underline text-emerald-400"
