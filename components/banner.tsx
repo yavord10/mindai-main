b234a4b0782dfe5c0b22ae13bd55a83a60597d66
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-05-31 22:06:57 +0100

    More changes

--- components/banner.tsx
+++ components/banner.tsx
@@ -8,7 +8,10 @@ export default function Banner() {
   return (
     <>
       {bannerOpen && (
-        <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-60">
+        <div
+          className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-60"
+          style={{ bottom: "100px", right: "120px" }}
+        >
           <div className="bg-slate-800 text-slate-50 text-sm p-3 shadow-lg flex justify-between rounded-lg">
             <div className="text-slate-500 inline-flex">
               <a
@@ -20,18 +23,6 @@ export default function Banner() {
                 Presale coming soon!
               </a>
             </div>
-            <button
-              className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700"
-              onClick={() => setBannerOpen(false)}
-            >
-              <span className="sr-only">Close</span>
-              <svg
-                className="w-4 h-4 shrink-0 fill-current"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.72 3.293a1 1 0 00-1.415 0L8.012 6.586 4.72 3.293a1 1 0 00-1.414 1.414L6.598 8l-3.293 3.293a1 1 0 101.414 1.414l3.293-3.293 3.293 3.293a1 1 0 001.414-1.414L9.426 8l3.293-3.293a1 1 0 000-1.414z" />
-              </svg>
-            </button>
           </div>
         </div>
       )}
